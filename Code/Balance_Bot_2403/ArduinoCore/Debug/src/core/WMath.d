de24646aefd67f12bb8b52191df0d52e52b7eb78
heethesh <heethesh@gmail.com>
2017-05-05 04:52:42 +0530

    Cleaned initial commit

--- Code/Balance_Bot_2403/ArduinoCore/Debug/src/core/WMath.d
+++
@@ -1,7 +0,0 @@
-src/core/WMath.d src/core/WMath.o: ../src/core/WMath.cpp \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h:
